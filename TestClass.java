f03e0595ef2fa98a8a089278076195351025662c
cianreilly99 <cianreilly1999@gmail.com>
2023-02-27 16:37:49 +0000

    my commit

--- TestClass.java
+++ TestClass.java
@@ -2,6 +2,8 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("hello.");
 
+        System.out.println("TEST");
+
         System.out.println("This is a test Java class.");
     }
 }
